2011403697f0fd926712efaf1d75aabeccce7a53
npc_unit <o_xx@list.ru>
2023-11-30 12:58:55 +0300

    added counter to header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,8 +5,13 @@ import { NavLink } from "react-router-dom";
 import { FiHeart } from "react-icons/fi";
 import { TbShoppingCartDollar } from "react-icons/tb";
 import classNames from "classnames";
+import { Counter } from "../counter/counter";
+import { useSelector } from "react-redux";
 
 export const Header = () => {
+  const { cart } = useSelector((store) => store.cart);
+  const { favorites } = useSelector((store) => store.favorites);
+
   return (
     <header className={styles.header}>
       <Logo />
@@ -14,20 +19,26 @@ export const Header = () => {
         <nav className={styles.header__nav}>
           <NavLink to={"/cart"} className={styles.header__link}>
             {({ isActive }) => (
-              <TbShoppingCartDollar
-                className={classNames(styles.header__icon, {
-                  [styles.header__icon_active]: isActive,
-                })}
-              />
+              <>
+                <Counter products={cart} />
+                <TbShoppingCartDollar
+                  className={classNames(styles.header__icon, {
+                    [styles.header__icon_active]: isActive,
+                  })}
+                />
+              </>
             )}
           </NavLink>
           <NavLink to={"/favorites"} className={styles.header__link}>
             {({ isActive }) => (
-              <FiHeart
-                className={classNames(styles.header__icon, {
-                  [styles.header__icon_active]: isActive,
-                })}
-              />
+              <>
+                <Counter products={favorites} />
+                <FiHeart
+                  className={classNames(styles.header__icon, {
+                    [styles.header__icon_active]: isActive,
+                  })}
+                />
+              </>
             )}
           </NavLink>
         </nav>
